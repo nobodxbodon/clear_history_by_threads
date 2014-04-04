2cd2f82143b977f4f99ebe249b8023672c24de7a
xuanwu <xuan@tangentlogic.com>
2014-04-04 13:51:30 -0700

    tested recursive deleting, works;need to add in HiT to keep the redirection nodes

--- chrome/content/deleteHistory.js
+++ chrome/content/deleteHistory.js
@@ -1,9 +1,36 @@
 com.wuxuan.fromwheretowhere.deleteHistory=function(){
   var pub={};
   
-  pub.delete=function(nodes){
-    for(var i=0;i<nodes.length;i++)
-      console.log("called in clear history by thread: "+nodes[i].label);
+  pub.browserHistory = Components.classes["@mozilla.org/browser/nav-history-service;1"]
+                     .getService(Components.interfaces.nsIBrowserHistory);
+                     
+                     
+  pub.delete=function(main){
+    var sel = main.getCurrentSelectedwithIndex();
+    
+    for(var i in sel){
+      console.log(JSON.stringify(sel[i]));
+      console.log("to delete:"+i+" open?"+sel[i].isFolded);
+      //fold it if not
+      if(sel[i].isFolded){
+        main.treeView.toggleOpenState(Number(i));
+      }
+      pub.deleteAll(sel[i]);
+      /*main.treeView.visibleData.splice(i,1);
+      main.treeView.treeBox.rowCountChanged(0,main.treeView.visibleData.length);*/
+    }
+    
+  };
+  
+  pub.deleteAll = function(node){
+    console.log("removeVisitsByTimeframe: "+node.label+" "+node.visit_date);
+    //pub.browserHistory.removeVisitsByTimeframe(node.visit_date,node.visit_date);
+    var ch = node.children;
+    if(ch==null)
+      return;
+    for(var i=0;i<ch.length;i++){
+      pub.deleteAll(ch[i]);
+    }
   };
   
   return pub;
